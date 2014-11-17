c1f6c4d561b580e47162accabfae4659c41efe84
AkanGulsen <akangulsen@hotmail.com>
2014-11-17 17:22:48 +0100

    GUI starter ved opstart af programmet
    Conflicts:
    	bin/GUI/UserList$1.class
    	bin/GUI/UserList$2.class
    	bin/GUI/UserList$3.class
    	bin/GUI/UserList$4.class
    	bin/GUI/UserList.class
    	bin/config/Configurations.class
    	bin/databaseMethods/SwitchMethods.class
    	src/config/Configurations.java

--- src/GUI/UserList.java
+++ src/GUI/UserList.java
@@ -218,7 +218,7 @@ public class UserList extends JPanel {
     
     public void addActionListener(ActionListener l) {
 //		btnAdd.addActionListener(l);
-		btnDelete.addActionListener(l);
+//		btnDelete.addActionListener(l);
 		btnLogout.addActionListener(l);
 		btnMainMenu.addActionListener(l);
 		
