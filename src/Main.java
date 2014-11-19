4c6d4d8fb1cd45707362e56dd041d214216ebf9d
Mads Vive <madsvive@gmail.com>
2014-11-19 16:13:00 +0100

    TCPServeren er added til main
    Programmet lytter nu på meddelelser fra Clienten

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,24 @@ import GUI.GUILogic;
 import config.Configurations;
 
 public class Main {
-	//Starts public main method.
-	
+	// Starts public main method.
+
 	public static void main(String[] args) {
 		Configurations cf = new Configurations();
-		
+
 		cf.ReadFile();
-		
+
 		System.out.println(cf.getPassword());
 		System.out.println(cf.getHost());
 		System.out.println(cf.getPort());
 		System.out.println(cf.getUsername());
 		System.out.println(cf.getDbname());
 		
+		//Runs GUI frame
 		new GUILogic().run();
+		//Runs the TCPServer witch listens for the client
+		new TCPServer();
+
 	}
 
 }
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,24 +1,30 @@
 import java.io.*;
 import java.net.*;
+import java.sql.SQLException;
+
 import JsonClasses.CalendarInfo;
 
 import com.google.gson.stream.JsonReader;
 
-class TCPServer{    
-	
-	public static void main(String argv[]) throws Exception       {
+class TCPServer {
 
-		AdminWorker admin = new AdminWorker();
-		//Creates a socket to send and recieve messages in port 8888
-		ServerSocket welcomeSocket = new ServerSocket(8888);
-		
-		//While something is true
-		while(true){
-			//Creates a socket and a buffered reader which recieves some sort of input from somewhere around the internet!
-			Socket connectionSocket = welcomeSocket.accept();
-			ClientWorker client= new ClientWorker(connectionSocket);
-			Thread thread = new Thread(client, "client");
-			thread.start();
+	public TCPServer() {
+		try {
+			AdminWorker admin = new AdminWorker();
+			// Creates a socket to send and recieve messages in port 8888
+			ServerSocket welcomeSocket = new ServerSocket(8888);
+
+			// While something is true
+			while (true) {
+				// Creates a socket and a buffered reader which recieves some
+				// sort of input from somewhere around the internet!
+				Socket connectionSocket = welcomeSocket.accept();
+				ClientWorker client = new ClientWorker(connectionSocket);
+				Thread thread = new Thread(client, "client");
+				thread.start();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
