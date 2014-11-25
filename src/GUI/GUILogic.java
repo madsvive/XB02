fff66b02cdbdf93055be1a8c7ea5a01a6e1d541f
Mads Vive <madsvive@gmail.com>
2014-11-25 18:38:30 +0100

    Lokal database benyttes
    og fejlrettelse i GUIlogic

--- src/GUI/GUILogic.java
+++ src/GUI/GUILogic.java
@@ -149,13 +149,13 @@ public class GUILogic {
 				{
 				QueryBuilder qb = new QueryBuilder();
 				
-				String[] kolonner = { "email", "password"};
+				String[] kolonner = {"email", "password"};
 				String[] Values = { Email, Password};
-				String[] kolonner2 = { "types"};
-				String[] Values2 = { Type};
+				String[] kolonner2 = {"type"};
+				String[] Values2 = {Type};
 				try {
-					qb.insertInto("users", kolonner ).values(Values).ExecuteQuery();
-					qb.insertInto("roles", kolonner ).values(Values).ExecuteQuery();
+					qb.insertInto("users", kolonner).values(Values).Execute();
+					qb.insertInto("roles", kolonner2).values(Values2).Execute();
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
