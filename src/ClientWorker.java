c7e7f88384388e6536011a488a1aaf3e112ec73f
Mads Vive <madsvive@gmail.com>
2014-12-07 23:35:43 +0100

    Forbindelse mellem client og Server
    Der forsøges stadig at sende mellem client og server. Har prøvet med
    Strings, String objects og Jsons (Virker ikke endnu)

--- src/ClientWorker.java
+++ src/ClientWorker.java
@@ -17,24 +17,53 @@ public class ClientWorker implements Runnable {
 	public void run() {
 		try {
 			System.out.println("forbindelse Oprettet!");
-			// BufferedReader inFromClient = new BufferedReader(new
-			// InputStreamReader(connectionSocket.getInputStream()));
-
 			byte[] b = new byte[500000];
 			int count = connectionSocketConected.getInputStream().read(b);
 			ByteArrayInputStream bais = new ByteArrayInputStream(b);
-			ObjectInputStream inFromClient = new ObjectInputStream(connectionSocketConected.getInputStream());
-			ObjectOutputStream outToClient = new ObjectOutputStream(connectionSocketConected.getOutputStream());
+			DataInputStream inFromClient = new DataInputStream(
+					connectionSocketConected.getInputStream());
+			// Creates an object of the data which is to be send back to the
+			// client, via the connectionSocket
+			DataOutputStream outToClient = new DataOutputStream(
+					connectionSocketConected.getOutputStream());
+			System.out.println("Outtoclient oprettet!");
+			// Sets client sentence equals input from client
+			// incomingJson = inFromClient.readLine();
 
-//			String ny = crypt.decrypt(b);
-			System.out.println(b);
-			String ny = b.toString();
+			String ny = crypt.decrypt(b);
+
+			// cryp.StringEncryption(inFromClient.readLine());
+			System.out.println("Besked modtaget!");
+			// Sysout recieved message
 			System.out.println("Received: " + ny);
 			String returnSvar = GS.GiantSwitchMethod(ny);
 			// Sends the capitalized message back to client!!
 			outToClient.writeBytes(returnSvar + "\n");
 			System.out.println("svar sendt");
 			// BufferedWriter writer = new BufferedWriter(arg0)
+
+			// System.out.println("forbindelse Oprettet!");
+			// // BufferedReader inFromClient = new BufferedReader(new
+			// // InputStreamReader(connectionSocket.getInputStream()));
+			//
+			// byte[] b = new byte[500000];
+			// int count = connectionSocketConected.getInputStream().read(b);
+			// ByteArrayInputStream bais = new ByteArrayInputStream(b);
+			// ObjectInputStream inFromClient = new
+			// ObjectInputStream(connectionSocketConected.getInputStream());
+			// ObjectOutputStream outToClient = new
+			// ObjectOutputStream(connectionSocketConected.getOutputStream());
+			//
+			// // String ny = crypt.decrypt(b);
+			// System.out.println(b);
+			// String ny = b.toString();
+			// System.out.println("Received: " + ny);
+			// String returnSvar = GS.GiantSwitchMethod(ny);
+			// // Sends the capitalized message back to client!!
+			// outToClient.writeBytes(returnSvar + "\n");
+			// System.out.println("svar sendt");
+			// // BufferedWriter writer = new BufferedWriter(arg0)
+
 		} catch (Exception exception) {
 			System.err.print(exception);
 			exception.printStackTrace();
